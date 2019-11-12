336c2db65e78239f99aa672b8e85a56d2f1496f9
irma1991 <55351564+irma1991@users.noreply.github.com>
2019-11-12 18:52:49 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,107 +3,168 @@
 let knyguSarasas =
 [
     {
-        'istorija':[
+        'Istorija':[
             {
                 ISBN: '9786098120462',
-                leidimoMetai: '2013',
-                pavadinimas: 'Karalienės Bonos virtuvė',
-                puslapiuSkaicius: '220'
+                Metai: '2013',
+                Pavadinimas: 'Karalienės Bonos virtuvė',
+                Puslapiai: '220'
             }
             ,
             {
                 ISBN: '9771392155005',
-                leidimoMetai: '2010',
-                pavadinimas: 'Kultūros paminklai 23',
-                puslapiuSkaicius: '208'
+                Metai: '2018',
+                Pavadinimas: 'Kultūros paminklai 23',
+                Puslapiai: '208'
             }
             ,
             {
                 ISBN: '9786094674044',
-                leidimoMetai: '2019',
-                pavadinimas: 'Lietuvos kultūros istorija',
-                puslapiuSkaicius: '356'
+                Metai: '2019',
+                Pavadinimas: 'Lietuvos kultūros istorija',
+                Puslapiai: '356'
             }
-            ,
-            {
-                ISBN: '9786094664373',
-                leidimoMetai: '2015',
-                pavadinimas: 'Kai Kaunas buvo Kaunas',
-                puslapiuSkaicius: '200'
-            }
+
         ]
     },
     {
-        'fantastika': [
+        'Fantastika': [
             {
                 ISBN: '9786094273902',
-                leidimoMetai: '2019',
-                pavadinimas: 'Amžinybės fjordo pranašai',
-                puslapiuSkaicius: '496'
+                Metai: '2019',
+                Pavadinimas: 'Amžinybės fjordo pranašai',
+                Puslapiai: '496'
             }
             ,
             {
                 ISBN: '9786094273872',
-                leidimoMetai: '2019',
-                pavadinimas: 'Fondas ir imperija',
-                puslapiuSkaicius: '328'
+                Metai: '2019',
+                Pavadinimas: 'Fondas ir imperija',
+                Puslapiai: '328'
             }
             ,
             {
                 ISBN: '9789955138303',
-                leidimoMetai: '2014',
-                pavadinimas: 'Sulaikyti žvaigždes',
-                puslapiuSkaicius: '336'
+                Metai: '2018',
+                Pavadinimas: 'Sulaikyti žvaigždes',
+                Puslapiai: '336'
             }
             ,
             {
                 ISBN: '9786090135631',
-                leidimoMetai: '2009',
-                pavadinimas: 'Smėlis',
-                puslapiuSkaicius: '336'
+                Metai: '2009',
+                Pavadinimas: 'Smėlis',
+                Puslapiai: '336'
+            }
+            ,
+            {
+                ISBN: '9786090135630',
+                Metai: '2019',
+                Pavadinimas: 'Žiema',
+                Puslapiai: '336'
             }
         ]
 
     },
     {   
-        'detektyvai': [
+        'Detektyvai': [
             {
                 ISBN: '9786090138823',
-                leidimoMetai:'2013' ,
-                pavadinimas: 'Avarinis nusileidimas',
-                puslapiuSkaicius: '464'
+                Metai:'2013' ,
+                Pavadinimas: 'Avarinis nusileidimas',
+                Puslapiai: '464'
             }
             ,
             {
                 ISBN: '9789955138556',
-                leidimoMetai: '2017',
-                pavadinimas: 'Gimę nusikalsti',
-                puslapiuSkaicius: '480'
+                Metai: '2017',
+                Pavadinimas: 'Gimę nusikalsti',
+                Puslapiai: '480'
             }
             ,
             {
                 ISBN: '9789955267065',
-                leidimoMetai: '2019',
-                pavadinimas: 'Gęstanti šviesa',
-                puslapiuSkaicius: '408'
+                Metai: '2018',
+                Pavadinimas: 'Gęstanti šviesa',
+                Puslapiai: '408'
             }
             ,
             {
                 ISBN: '9786094443046',
-                leidimoMetai: '2012',
-                pavadinimas: 'Diena, kai įvyko nelaimė',
-                puslapiuSkaicius: '320'
+                Metai: '2012',
+                Pavadinimas: 'Diena, kai įvyko nelaimė',
+                Puslapiai: '320'
+            }
+            ,            {
+                ISBN: '9786094443096',
+                Metai: '2019',
+                Pavadinimas: 'Mano laimė',
+                Puslapiai: '320'
+            }
+            ,
+                        {
+                ISBN: '9786094443088',
+                Metai: '2012',
+                Pavadinimas: 'Pavasaris',
+                Puslapiai: '320'
             }
         ]
     }
 ]
 
-
-for (let knyguKategorija of knyguSarasas)
+for (let knyguKategorija of knyguSarasas) // prieinam prie knygu kategorijos
 {
-  console.log( knyguKategorija ); // prieinam prie knygu kategorijos
-        for (let knyga in knyguKategorija)
+        for (let knygos in knyguKategorija)
         {
-            console.log( knyga ); // prieinam prie atskiros knygos
+            console.log( "Knygu kategorija: " + knygos + " (" + knyguKategorija[knygos].length + " knygos)")// prieinam prie atskiros knygos
+
+                for (let knyga in knygos)
+                {   console.log(" ")
+                    for (let knygosInfo in knyguKategorija[knygos][knyga])
+                    {
+                        if(knygosInfo != 'Metai')
+                        {
+                            console.log(knygosInfo+ ' : ' + knyguKategorija[knygos][knyga][knygosInfo])
+                        }
+                        if(knygosInfo == 'Metai')
+                        {
+                            let condition = knyguKategorija[knygos][knyga][knygosInfo] == '2019' ? ' (nauja knyga)' : ''
+                            console.log(knygosInfo + ' : ' + knyguKategorija[knygos][knyga][knygosInfo]+condition)
+                        }
+
+                    }
+                }
         }
 }
+
+                        // for (let kategorija in knyguSarasas)
+                        // {
+                        //     for (let i=0; i<knyguSarasas[kategorija].length; i++)
+                        //     {
+                        //         if(knyguSarasas[kategorija][i]['Metai']==2018)
+                        //         {
+                        //             console.log("Kategorija: " + kategorija + " (" + knygos[kategorija].length + ") ");
+                        //             for(let savybe in knygos[kategorija][i])
+                        //             {
+                        //                 console.log(vertimai[savybe] + ": " + knygos[kategorija][i][savybe]);
+                        //             }
+                        //             console.log("/n");
+                        //         }
+                        //     }
+                        // }
+
+                        // for (let kategorija in knygos)
+                        // {
+                        //     for (let i=0; i<knygos[kategorija].length; i++)
+                        //     {
+                        //         if(knygos[kategorija][i]['year']==2018)
+                        //         {
+                        //             console.log("Kategorija: " + kategorija + " (" + knygos[kategorija].length + ") ");
+                        //             for(let savybe in knygos[kategorija][i])
+                        //             {
+                        //                 console.log(vertimai[savybe] + ": " + knygos[kategorija][i][savybe]);
+                        //             }
+                        //             console.log("/n");
+                        //         }
+                        //     }
+                        // }
