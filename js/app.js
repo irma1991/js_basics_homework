e69d95a51dcf138c49fb90a77c398503053fce80
Irma Suchonickaja <irmalinartaite@gmail.com>
2019-11-17 22:00:58 +0200

    files

--- js/app.js
+++ js/app.js
@@ -8,21 +8,24 @@ let knyguSarasas =
                 ISBN: '9786098120462',
                 Metai: '2013',
                 Pavadinimas: 'Karalienės Bonos virtuvė',
-                Puslapiai: '220'
+                Puslapiai: '220',
+                Kaina: '10.235'
             }
             ,
             {
                 ISBN: '9771392155005',
                 Metai: '2018',
                 Pavadinimas: 'Kultūros paminklai 23',
-                Puslapiai: '208'
+                Puslapiai: '208',
+                Kaina: '5.745'
             }
             ,
             {
                 ISBN: '9786094674044',
                 Metai: '2019',
                 Pavadinimas: 'Lietuvos kultūros istorija',
-                Puslapiai: '356'
+                Puslapiai: '356',
+                Kaina: '8.254'
             }
 
         ]
@@ -33,35 +36,40 @@ let knyguSarasas =
                 ISBN: '9786094273902',
                 Metai: '2019',
                 Pavadinimas: 'Amžinybės fjordo pranašai',
-                Puslapiai: '496'
+                Puslapiai: '496',
+                Kaina: '18.347'
             }
             ,
             {
                 ISBN: '9786094273872',
                 Metai: '2019',
                 Pavadinimas: 'Fondas ir imperija',
-                Puslapiai: '328'
+                Puslapiai: '328',
+                Kaina: '7.247'
             }
             ,
             {
                 ISBN: '9789955138303',
                 Metai: '2018',
                 Pavadinimas: 'Sulaikyti žvaigždes',
-                Puslapiai: '336'
+                Puslapiai: '336',
+                Kaina: '9.254'
             }
             ,
             {
                 ISBN: '9786090135631',
                 Metai: '2009',
                 Pavadinimas: 'Smėlis',
-                Puslapiai: '336'
+                Puslapiai: '336',
+                Kaina: '18.417'
             }
             ,
             {
                 ISBN: '9786090135630',
                 Metai: '2019',
                 Pavadinimas: 'Žiema',
-                Puslapiai: '336'
+                Puslapiai: '336',
+                Kaina: '7.548'
             }
         ]
 
@@ -72,41 +80,48 @@ let knyguSarasas =
                 ISBN: '9786090138823',
                 Metai:'2013' ,
                 Pavadinimas: 'Avarinis nusileidimas',
-                Puslapiai: '464'
+                Puslapiai: '464',
+                Kaina: '17.254'
             }
             ,
             {
                 ISBN: '9789955138556',
                 Metai: '2017',
                 Pavadinimas: 'Gimę nusikalsti',
-                Puslapiai: '480'
+                Puslapiai: '480',
+                Kaina: '9.254'
             }
             ,
             {
                 ISBN: '9789955267065',
                 Metai: '2018',
                 Pavadinimas: 'Gęstanti šviesa',
-                Puslapiai: '408'
+                Puslapiai: '408',
+                Kaina: '16.247'
             }
             ,
             {
                 ISBN: '9786094443046',
                 Metai: '2012',
                 Pavadinimas: 'Diena, kai įvyko nelaimė',
-                Puslapiai: '320'
+                Puslapiai: '320',
+                Kaina: '14.457'
             }
-            ,            {
+            ,
+            {
                 ISBN: '9786094443096',
                 Metai: '2019',
                 Pavadinimas: 'Mano laimė',
-                Puslapiai: '320'
+                Puslapiai: '320',
+                Kaina: '5.247'
             }
             ,
-                        {
+            {
                 ISBN: '9786094443088',
                 Metai: '2012',
                 Pavadinimas: 'Pavasaris',
-                Puslapiai: '320'
+                Puslapiai: '320',
+                Kaina: '8.254'
             }
         ]
     }
@@ -116,55 +131,55 @@ for (let knyguKategorija of knyguSarasas) // prieinam prie knygu kategorijos
 {
         for (let knygos in knyguKategorija)
         {
-            console.log( "Knygu kategorija: " + knygos + " (" + knyguKategorija[knygos].length + " knygos)")// prieinam prie atskiros knygos
-
-                for (let knyga in knygos)
-                {   console.log(" ")
-                    for (let knygosInfo in knyguKategorija[knygos][knyga])
+            for (let i = 0; i<knyguKategorija[knygos].length; i++)
+            {
+                if(knyguKategorija[knygos][i]['Kaina'])
+                {
+                    console.log("Kategorija: "+ knygos);
+                    for (let savybe in knyguKategorija[knygos][i])
                     {
-                        if(knygosInfo != 'Metai')
-                        {
-                            console.log(knygosInfo+ ' : ' + knyguKategorija[knygos][knyga][knygosInfo])
-                        }
-                        if(knygosInfo == 'Metai')
-                        {
-                            let condition = knyguKategorija[knygos][knyga][knygosInfo] == '2019' ? ' (nauja knyga)' : ''
-                            console.log(knygosInfo + ' : ' + knyguKategorija[knygos][knyga][knygosInfo]+condition)
-                        }
-
+                        console.log(Math.max(savybe + ": " + knyguKategorija[knygos][i][savybe]));
                     }
-                }
+                } 
+            }
         }
-}
 
-                        // for (let kategorija in knyguSarasas)
-                        // {
-                        //     for (let i=0; i<knyguSarasas[kategorija].length; i++)
-                        //     {
-                        //         if(knyguSarasas[kategorija][i]['Metai']==2018)
-                        //         {
-                        //             console.log("Kategorija: " + kategorija + " (" + knygos[kategorija].length + ") ");
-                        //             for(let savybe in knygos[kategorija][i])
-                        //             {
-                        //                 console.log(vertimai[savybe] + ": " + knygos[kategorija][i][savybe]);
-                        //             }
-                        //             console.log("/n");
-                        //         }
-                        //     }
-                        // }
+// for (let knyguKategorija of knyguSarasas) // prieinam prie knygu kategorijos
+// {
+//         for (let knygos in knyguKategorija)
+//         {
+//             for (let i = 0; i<knyguKategorija[knygos].length; i++)
+//             {
+//                 if(knyguKategorija[knygos][i]['Metai']==2018)
+//                 {
+//                     console.log(" ")
+//                     console.log("Kategorija: "+ knygos);
+//                     for (let savybe in knyguKategorija[knygos][i])
+//                     {
+//                         console.log(savybe + ": " + knyguKategorija[knygos][i][savybe]);
+//                     }
+//                 } 
+//             }
+//         }
 
-                        // for (let kategorija in knygos)
-                        // {
-                        //     for (let i=0; i<knygos[kategorija].length; i++)
-                        //     {
-                        //         if(knygos[kategorija][i]['year']==2018)
-                        //         {
-                        //             console.log("Kategorija: " + kategorija + " (" + knygos[kategorija].length + ") ");
-                        //             for(let savybe in knygos[kategorija][i])
-                        //             {
-                        //                 console.log(vertimai[savybe] + ": " + knygos[kategorija][i][savybe]);
-                        //             }
-                        //             console.log("/n");
-                        //         }
-                        //     }
-                        // }
+            // console.log( "Knygu kategorija: " + knygos + " (" + knyguKategorija[knygos].length + " knygos)")// prieinam prie atskiros knygos
+
+            //     for (let knyga in knygos)
+            //     { console.log(" ")
+            //         for (let knygosInfo in knyguKategorija[knygos][knyga])
+            //         {
+
+
+            //             if(knygosInfo != 'Metai')
+            //             {
+            //                 console.log(knygosInfo+ ' : ' + knyguKategorija[knygos][knyga][knygosInfo])
+            //             }
+            //             if(knygosInfo == 'Metai')
+            //             {
+            //                 let condition = knyguKategorija[knygos][knyga][knygosInfo] == '2019'
+            //                 console.log(knygosInfo + ' : ' + knyguKategorija[knygos][knyga][knygosInfo]+condition)
+            //             }
+
+            //         }
+            //     }
+        }
